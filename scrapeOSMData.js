b6e33ba330434259c5464adca12a02adb1a529c8
curran <curran.kelleher@gmail.com>
2018-11-13 16:35:32 +0530

    Scrape OSM data into geojson

--- scrapeOSMData.js
+++ scrapeOSMData.js
@@ -2,6 +2,11 @@
 // To run this, execute 'npm run scrapeOSMData'.
 // ESM is used so that we can use ES6 modules here.
 import request from 'request';
+import fs from 'fs';
+import { DOMParser } from 'xmldom';
+import osmtogeojson from 'osmtogeojson';
+
+const output = './public/geo.json';
 
 // Geographic bounding box for which to scrape data.
 // To discover this, use OpenStreetMap to navigate to the area,
@@ -14,10 +19,14 @@ const geoBoundingBox = '-71.1123,42.3526,-71.0786,42.3672';
 const apiBase = 'https://overpass-api.de/api/map';
 const url = `${apiBase}?bbox=${geoBoundingBox}`;
 
-console.log('here');
-request(url, (error, response, body) => {
+request(url, (error, response, xml) => {
   if (error) {
     console.log(error);
   }
-  const osmXML = body;
+  const parser = new DOMParser();
+  const dom = parser.parseFromString(xml, 'text/xml');
+  const geo = osmtogeojson(dom);
+  const json = JSON.stringify(geo);
+
+  fs.writeFileSync(output, json);
 });
